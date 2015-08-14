c31e7236f5a60f15eb59e45acf7b0a6a21ea47ea
everybody- <roux@thecrunge.org>
2015-08-14 12:07:56 -0400

    Cleanup

--- Programming Exercises/codeabbey.com/smoothing the weather.py
+++ Programming Exercises/codeabbey.com/smoothing the weather.py
@@ -1,9 +1,7 @@
 m= map(float, raw_input("Enter the measurements:\n").split())
 smoothed = list(m)
-print m
-print smoothed
 
 for n in range(1, len(m)-1):
     smoothed[n] = ((m[n-1] + m[n] + m[n+1]) / 3)
-print "m: ", m
+
 print smoothed
